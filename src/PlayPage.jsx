4e472830c1985b6039a82eec0114333f61abbfc1
rsterenchak <rsterenchak@gmail.com>
2024-01-21 02:17:59 -0800

    updated second css file for PlayPage

--- src/PlayPage.jsx
+++ src/PlayPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css';
+import './style2.css';
 import musicIcon from './assets/musical-notes.svg'
 import gitIcon from './assets/github.svg'
 import fightButton from './assets/FightButton.svg'
@@ -19,65 +19,60 @@ export default function PlayPage() {
 
     <div className='outerSection'>
           
-    <div className='navSection'>
+      <div className='navSection'>
 
-      <div className='topColumn1'>
+        <div className='topColumn1'>
 
-          <div className='musicBlock'>
-            
-            <img className='musicIcon' src={musicIcon} ></img>
+            <div className='musicBlock'>
+              
+              <img className='musicIcon' src={musicIcon} ></img>
 
-          </div>
+            </div>
 
-
-      </div>
-      <div className='topColumn2'>
-
-
-          <div className='portfolioBlock'>
-
-            <div className='portfolioText'>@rsterenchak</div>
-
-            <div className='portfolioIcon'>
-
-              <img className='gitIcon' src={gitIcon}></img>
+            <div className='musicBlock2'>
+              
+              <img className='musicIcon2' src={musicIcon} ></img>
 
             </div>
 
 
-          </div>
+        </div>
+        <div className='topColumn2'>
+
+
+            <div className='portfolioBlock'>
+
+              <div className='portfolioText'>@rsterenchak</div>
+
+              <div className='portfolioIcon'>
+
+                <img className='gitIcon' src={gitIcon}></img>
+
+              </div>
+
+
+            </div>
+
+
+        </div>
 
 
       </div>
+      
+      <div className='logoSection'>
 
 
-    </div>
-    
-    <div className='logoSection'>
-
-      <div className='logoContainer'></div>
-
-    </div>
-    
-    <div className='logoSection2'>
-
-      <div className='logoContainer2'></div>
+      </div>
+      
+      <div className='logoSection2'>
 
 
-    </div>
-    
-    <div className='inputSection'>
-
-      <div className='fightButton'>
-
-      <svg className='svg-element' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30">
-        <text x="50%" y="50%" className="svg-text" alignmentBaseline="middle" textAnchor="middle">Fight</text>
-      </svg>
+      </div>
+      
+      <div className='inputSection'>
 
       </div>
 
     </div>
-
-  </div>
   );
 }
