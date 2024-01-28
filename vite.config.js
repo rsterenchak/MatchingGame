cc78f088ae5a436da4663918e7dd30906d9cf08d
rsterenchak <rsterenchak@gmail.com>
2024-01-28 03:17:35 -0800

    Converted build from create-react to vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths'
+
+export default defineConfig ({
+
+  plugins: [react(), jsconfigPaths()],
+  server: {
+    port: 3000,
+    open: '/'
+  },
+  preview: {
+	port: 3000
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+    	additionalData: `@import "src/components/global.scss";`
+      }
+    }
+  }
+});
